541771ae136895a584c3a7c72fc43e0894e278f2
Cassidy Clawson <cassidyclawson@gmail.com>
2014-08-07 15:26:06 -0700

    added placeholder controller routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,30 @@
 get '/' do
   haml :index
 end
+
+get '/users' do #list of all users
+end
+
+post or put '/users/login' do #route for logging in
+end
+
+get '/users/new' do #form to create a new user
+end
+
+get '/users/:color' do #display all of a users splashes
+end
+
+get '/users/edit' do #form to modify profile
+end
+
+get '/splashes' do #display all splashes
+end
+
+get '/splashes/:id' do #display a particular splash
+end
+
+get '/splashes/:id/delete' do #deletes a splash (post or get?)
+end
+
+get '/splashes/new' do #display splash creation
+end
