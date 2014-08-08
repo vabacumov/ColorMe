3c44c7a5e17ed5fb4809082d5ae7baee2fcaa21a
Vlad Abacumov <vabacumov@gmail.com>
2014-08-08 11:00:08 -0700

    modifies splash model to create a self-join association splash now has methods #resplashes and #original_splash

--- app/models/splash.rb
+++ app/models/splash.rb
@@ -1,3 +1,8 @@
 class Splash < ActiveRecord::Base
   belongs_to :user
+
+  has_many :resplashes, class_name: "Splash",
+    foreign_key: "original_splash_id"
+  belongs_to :original_splash, class_name: "Splash",
+    foreign_key: "original_splash_id"
 end
